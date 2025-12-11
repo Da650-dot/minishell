18be3365aa52dbc6bcaeee24dff538e1a9ea6761
Danilo De Sousa Oliveira <dde-sou2@c1r10p2.42sp.org.br>
2025-12-11 16:39:53 -0300

    ajuste da mensagem PWD (command not found) para o correto (Is a direcory)

--- src/executor/exec_external.c
+++ src/executor/exec_external.c
@@ -6,7 +6,7 @@
 /*   By: dde-sou2 <danilo.bleach12@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 16:51:26 by dde-sou2          #+#    #+#             */
-/*   Updated: 2025/12/09 18:03:03 by dde-sou2         ###   ########.fr       */
+/*   Updated: 2025/12/11 16:37:26 by dde-sou2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,21 @@ char	*resolve_command_path(char *cmd, t_data *data)
 	return (result);
 }
 
+static void	print_command_error(char *cmd_name)
+{
+	struct stat	statbuf;
+
+	if (stat(cmd_name, &statbuf) == 0)
+	{
+		if (S_ISDIR(statbuf.st_mode))
+		{
+			print_error(cmd_name, NULL, "Is a directory");
+			return ;
+		}
+	}
+	print_error(cmd_name, NULL, "command not found");
+}
+
 int	execute_external(t_cmd *cmd, t_data *data)
 {
 	t_exec_args	exec_args;
@@ -33,7 +48,7 @@ int	execute_external(t_cmd *cmd, t_data *data)
 	path = resolve_command_path(cmd->args[0], data);
 	if (!path)
 	{
-		print_error(cmd->args[0], NULL, "command not found");
+		print_command_error(cmd->args[0]);
 		return (127);
 	}
 	exec_args.path = path;
